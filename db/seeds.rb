a4fd30401c92c1c632dfb0f9d03919c6887e9fdb
Tragg0r <d.schmidt86@outlook.com>
2016-12-30 12:20:16 +0100

    added styles for product page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(:name => "bike 1", :description => "A red bike!", :color => "red")
+Product.create(:name => "bike 2", :description => "A black bike!", :color => "black")
+Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!", :color => "yellow")
