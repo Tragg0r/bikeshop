5f9e8eb5cdf42e3da9869e280d5f2e2feff76ae9
Tragg0r <d.schmidt86@outlook.com>
2017-01-15 18:15:03 +0100

    5.3 added mail function to my app

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,3 +2,13 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 end
+
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'd.schmidt86@outlook.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+end
