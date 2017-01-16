6f808c166fadc6dfbc981abd22c7070fedd9e53d
Tragg0r <d.schmidt86@outlook.com>
2017-01-16 20:05:15 +0100

    4.6 added the product image to the product show page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,4 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
-end
-
-class StaticPagesController < ApplicationController
 	def landing_page
    		@products = Product.limit(4)
   	end
