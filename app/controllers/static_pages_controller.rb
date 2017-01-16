81d210428174d45368cb605d7fbfe00418d0a926
Tragg0r <d.schmidt86@outlook.com>
2017-01-16 15:01:10 +0100

    5.3 added mail function to the contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,19 @@ end
 
 class StaticPagesController < ApplicationController
 	def landing_page
-   	@products = Product.limit(4)
+   		@products = Product.limit(4)
   	end
+end
+
+class StaticPagesController < ApplicationController
+	def thank_you
+  		@name = params[:name]
+  		@email = params[:email]
+  		@message = params[:message]
+  		ActionMailer::Base.mail(:from => @email,
+      		:to => 'd.schmidt86@outlook.com',
+      		:subject => "A new contact form message from #{@name}",
+      		:body => @message).deliver_now
+end
+
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,14 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'd.schmidt86@outlook.com'
   layout 'mailer'
 end
 
-def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(:from => @email,
-      :to => 'd.schmidt86@outlook.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
-end
