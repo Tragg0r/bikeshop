36f0a7dba19ca9f13625fdf31860426512fd661e
Tragg0r <d.schmidt86@outlook.com>
2017-01-17 18:53:04 +0100

    4.6 cleaned up the StaticPagesController

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,8 @@
 class StaticPagesController < ApplicationController
 	def landing_page
    		@products = Product.limit(4)
-  	end
-end
+  end
 
-class StaticPagesController < ApplicationController
 	def thank_you
   		@name = params[:name]
   		@email = params[:email]
@@ -13,6 +11,6 @@ class StaticPagesController < ApplicationController
       		:to => 'd.schmidt86@outlook.com',
       		:subject => "A new contact form message from #{@name}",
       		:body => @message).deliver_now
-end
+  end
 
 end
