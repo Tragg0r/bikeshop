ebfa63b3b9322e242648705fdc4b70d0782931b6
Tragg0r <d.schmidt86@outlook.com>
2017-03-19 13:31:03 +0100

    deleted some lines

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,11 +16,7 @@ describe UsersController, :type => :controller do
 
        it "loads correct user details" do
 
-<<<<<<< HEAD
         get :show, params: {id: @user.id}
-=======
-        get :show, params:{id: @user.id}
->>>>>>> 0a6ea5a7faef54d8afe0b71fd0d17e0ed052ec6a
 
         expect(response).to have_http_status(200)
         expect(assigns(:user)).to eq @user
@@ -30,33 +26,24 @@ describe UsersController, :type => :controller do
 
      context 'No user is logged in' do
        it 'redirects to login' do
-<<<<<<< HEAD
+
          get :show, id: @user.id
          expect(response).to redirect_to(new_user_session_path)
-=======
-         get :show, id: user.id
-         expect(response).to redirect_to(root_path)
->>>>>>> 0a6ea5a7faef54d8afe0b71fd0d17e0ed052ec6a
+
        end
      end
 
      context "Current User is not allowed" do
 
        before do
-<<<<<<< HEAD
+
          sign_in @user2
-=======
-         sign_in @user
->>>>>>> 0a6ea5a7faef54d8afe0b71fd0d17e0ed052ec6a
+
        end
 
        it "to see other users details" do
 
-<<<<<<< HEAD
          get :show, id: @user.id
-=======
-         get :show, params:{id: @user2.id}
->>>>>>> 0a6ea5a7faef54d8afe0b71fd0d17e0ed052ec6a
 
          expect(response).to have_http_status(302)
          expect(response).to redirect_to(root_path)
