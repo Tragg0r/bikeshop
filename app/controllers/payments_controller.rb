92a43096e9b2e5c0bb09185d11ddff7f3a928320
Tragg0r <d.schmidt86@outlook.com>
2017-04-02 15:19:08 +0200

    6.6 added payments function

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,40 @@
+class PaymentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    token = params[:stripeToken]
+    # Create the charge on Stripe's servers - this will charge the user's card
+    @product = Product.find (params[:product_id])
+    @user = current_user
+
+    begin
+      charge = Stripe::Charge.create(
+        :amount => @product.price*100, # amount in cents, again
+        :currency => "usd",
+        :source => token,
+        :description => params[:stripeEmail]
+        )
+
+        if charge.paid
+          Order.create(
+            product_id: @product.id,
+            user_id: @user.id,
+            total: @product.price_in_cents
+        )
+
+        # UserMailer.order_recieved(@user).deliver_now
+
+        end
+
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+
+    redirect_to product_path(@product), notice: "Your payment has been processed!"
+
+  end
+
+end
