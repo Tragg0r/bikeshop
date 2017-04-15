a1459e51b2ff550a3296f9ea328db08a51b9aa75
Tragg0r <d.schmidt86@outlook.com>
2017-04-15 19:25:05 +0200

    6.10 added some AJAX for the product comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,14 @@
-$(document).on('turbolinks:load', function(){
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
     //Raty Plugin
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' } );
     $('.rated').raty({ path: '/assets',
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,8 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
-      else
+				format.js
+    	else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
