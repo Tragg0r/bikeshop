83a588353728da44cbdd6df6b64841b4433ea768
Tragg0r <d.schmidt86@outlook.com>
2017-04-17 14:21:16 +0200

    6.6 changed stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-    :secret_key => ENV['STRIPE_SECRET_KEY']
+    :publishable_key => ENV['pk_test_2h3drgxBAxa5GbVSs9MLYW3q'],
+    :secret_key => ENV['sk_test_hQT5NKioyqEJdlUBPlZHsAMg']
   }
 else
   Rails.configuration.stripe = {
